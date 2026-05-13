8b067e0ec4e83225bfa83bd74dda295e4ad40b54
leonardo-dsouza <dkaireturn@gmail.com>
2026-05-13 08:44:58 -0300

    fix prisma config

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'CloudVault API v1.0.0 - running';
+    return 'StackMesh API v1.0.0 - running';
   }
 }
