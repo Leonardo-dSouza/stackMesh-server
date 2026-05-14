487ff9dfc82b1c6f64abfd5cfd5439437117eca5
leonardo-dsouza <dkaireturn@gmail.com>
2026-05-14 09:34:19 -0300

    add compatibilidade com minio

--- src/s3/s3.service.ts
+++ src/s3/s3.service.ts
@@ -8,11 +8,12 @@ export class S3Service {
 
   constructor() {
     this.s3Client = new S3Client({
-      region: process.env.AWS_REGION || 'us-east-1',
+      region: process.env.AWS_REGION,
+      endpoint: process.env.AWS_S3_ENDPOINT,
+      forcePathStyle: true,
       credentials: {
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-        sessionToken: process.env.AWS_SESSION_TOKEN,
       },
     });
   }
